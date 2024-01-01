f0c1cebeeaf8db59c5469406b0ae3c681faeed34
Anirudh Subramanian <66243256+AnirudhSub@users.noreply.github.com>
2024-01-01 16:50:59 +0530

    rock / paper /scissors buttons working
    They work ! Open the console to see the stuff

--- index.js
+++ index.js
@@ -23,33 +23,61 @@ function playRound(playerSelection , computerSelection) {
     else if (playerSelection == computerSelection)
       return "Tie"
     else
-      return "Lose" //
+      return "Lose" 
 }
 
 
 const btn = document.querySelector('#btn');
 btn.onclick = startGame;
 
-function startGame(){
-  var playerWins = 0 , computerWins = 0 
-  while( playerWins != 5 && computerWins !=5){
-    var playerSelection = getPlayerSelection()
-    var computerSelection = getComputerChoice()
-    var result = playRound(playerSelection , computerSelection)
-    if(result == "Tie"){
-      console.log("Tie happened , reroll")
-    }
-    if(result=="Win"){
-      playerWins = playerWins + 1
-      console.log("You won! you chose " + playerSelection + " and the computer chose " + computerSelection + " !")
-    }
-    else{
-      computerWins = computerWins + 1
-      console.log("You lost ! you chose " + playerSelection + "and the computer chose " + computerSelection+ " !")
-    }     
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+
+rockBtn.onclick = function() {
+  makeSelection("rock");
+};
+paperBtn.onclick = function() {
+  makeSelection("paper");
+};
+scissorsBtn.onclick = function() {
+  makeSelection("scissor");
+};
+
+function makeSelection(playerSelection) {
+  var computerSelection = getComputerChoice();
+  var result = playRound(playerSelection, computerSelection);
+  console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}`);
+
+  if (result === "Tie") {
+    console.log("Tie happened, reroll");
+  } else if (result === "Win") {
+    console.log("You won!");
+  } else {
+    console.log("You lost!");
   }
-  if(playerWins == 5 ) console.log("Congratulations! you won five rounds!")
-  else console.log("Tough luck! Try again ")
+}
+
+function startGame(){
+  // var playerWins = 0 , computerWins = 0 
+  // while( playerWins != 5 && computerWins !=5){
+  //   var playerSelection = getPlayerSelection()
+  //   var computerSelection = getComputerChoice()
+  //   var result = playRound(playerSelection , computerSelection)
+  //   if(result == "Tie"){
+  //     console.log("Tie happened , reroll")
+  //   }
+  //   if(result=="Win"){
+  //     playerWins = playerWins + 1
+  //     console.log("You won! you chose " + playerSelection + " and the computer chose " + computerSelection + " !")
+  //   }
+  //   else{
+  //     computerWins = computerWins + 1
+  //     console.log("You lost ! you chose " + playerSelection + "and the computer chose " + computerSelection+ " !")
+  //   }     
+  // }
+  // if(playerWins == 5 ) console.log("Congratulations! you won five rounds!")
+  // else console.log("Tough luck! Try again ")
   return 0
 }
 
